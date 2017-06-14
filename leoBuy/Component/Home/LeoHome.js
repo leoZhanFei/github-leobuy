1d60991c1659c61f321aeecf5004113aacae52f8
leozhangfei <zhangfei@ihandy.cn>
2017-06-14 17:39:03 +0800

    首页修改
    首页修改

--- leoBuy/Component/Home/LeoHome.js
+++ leoBuy/Component/Home/LeoHome.js
@@ -8,19 +8,38 @@ import React, { Component } from 'react';
 import {
     StyleSheet,
     Text,
-    View
+    View,
+    TextInput
 } from 'react-native';
 
 var Home = React.createClass({
     render() {
         return (
             <View style={styles.container}>
+                {/*首页导航条*/}
+                {this.renderNavigatorBar()}
                 <Text style={styles.welcome}>
                     首页
                 </Text>
             </View>
         );
-    }
+    },
+
+    // 首页导航条
+    renderNavigatorBar(){
+        return (
+            <View>
+                {/*左边*/}
+                <Text>上海</Text>
+                {/*中间*/}
+                <TextInput
+                    placeholder='输入商家、品类、商圈'
+                />
+                {/*右边*/}
+            </View>
+        )
+    },
+
 });
 
 const styles = StyleSheet.create({
